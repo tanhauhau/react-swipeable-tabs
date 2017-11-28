a884e8f7b5f5659fedc49eb78680347536397899
Li Hau Tan <tanlh@seagroup.com>
2017-11-28 17:04:53 +0800

    safety

--- src/dom-utils.js
+++ src/dom-utils.js
@@ -1,11 +1,15 @@
 export function setTransform(node, transform) {
-  node.style.transform = transform;
-  node.style.WebkitTransform = transform;
+  if (node) {
+    node.style.transform = transform;
+    node.style.WebkitTransform = transform;
+  }
 }
 
 export function setTransition(node, transition) {
-  node.style.transition = transition;
-  node.style.WebkitTransition = transition;
+  if (node) {
+    node.style.transition = transition;
+    node.style.WebkitTransition = transition;
+  }
 }
 
 let supportsPassive = false;
